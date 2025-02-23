39b0f0468b42ed045983caf0e314ab9c3faaefc7
AzRyCb <azry695@gmail.com>
2025-02-23 12:13:16 +0700

    Update message.js

--- message.js
+++ message.js
@@ -106,49 +106,19 @@ By: https://github.com/DikaArdnt/readsw\n\n`;
 ${readMore}
 💻 *_Info Server_*
 *- RAM :* ${Func.formatSize(os.totalmem() - os.freemem(), false)} / ${Func.formatSize(os.totalmem(), false)}
+*- Runtime:* ${Func.runtime(process.uptime())}
 
-*Runtime Bot:*
-${Func.runtime(process.uptime())}
+_NodeJS Memory Usaage_
+${Object.keys(used).map((key, _, arr) => `${key.padEnd(Math.max(...arr.map(v=>v.length)),' ')}: ${Func.formatSize(used[key])}`).join('\n')}
 
-*_NodeJS Memory Usage_*
-${Object.keys(used)
-	.map((key, _, arr) => `*- ${key.padEnd(Math.max(...arr.map(v => v.length)), ' ')} :* ${Func.formatSize(used[key])}`)
-	.join('\n')}
-*- Heap Executable :* ${Func.formatSize(heapStat?.total_heap_size_executable)}
-*- Physical Size :* ${Func.formatSize(heapStat?.total_physical_size)}
-*- Available Size :* ${Func.formatSize(heapStat?.total_available_size)}
-*- Heap Limit :* ${Func.formatSize(heapStat?.heap_size_limit)}
-*- Malloced Memory :* ${Func.formatSize(heapStat?.malloced_memory)}
-*- Peak Malloced Memory :* ${Func.formatSize(heapStat?.peak_malloced_memory)}
-*- Does Zap Garbage :* ${Func.formatSize(heapStat?.does_zap_garbage)}
-*- Native Contexts :* ${Func.formatSize(heapStat?.number_of_native_contexts)}
-*- Detached Contexts :* ${Func.formatSize(heapStat?.number_of_detached_contexts)}
-*- Total Global Handles :* ${Func.formatSize(heapStat?.total_global_handles_size)}
-*- Used Global Handles :* ${Func.formatSize(heapStat?.used_global_handles_size)}
-${
-	cpus[0]
-		? `
-
-*_Total CPU Usage_*
-${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times)
-				.map(type => `*- ${(type + '*').padEnd(6)}: ${((100 * cpu.times[type]) / cpu.total).toFixed(2)}%`)
-				.join('\n')}
-
-*_CPU Core(s) Usage (${cpus.length} Core CPU)_*
-${cpus
-	.map(
-		(cpu, i) =>
-			`${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times)
-				.map(type => `*- ${(type + '*').padEnd(6)}: ${((100 * cpu.times[type]) / cpu.total).toFixed(2)}%`)
-				.join('\n')}`
-	)
-	.join('\n\n')}`
-		: ''
-}
-`.trim();
-                await m.reply(teks);
-            }
-            break;
+${cpus[0] ? `_Total CPU Usage_
+${cpus[0].model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}
+_CPU Core(s) Usage (${cpus.length} Core CPU)_
+${cpus.map((cpu, i) => `${i + 1}. ${cpu.model.trim()} (${cpu.speed} MHZ)\n${Object.keys(cpu.times).map(type => `- *${(type + '*').padEnd(6)}: ${(100 * cpu.times[type] / cpu.total).toFixed(2)}%`).join('\n')}`).join('\n\n')}` : ''}
+                `.trim();
+					await m.reply(teks);
+				}
+				break;
 
             case 'myrvo':
                 if (!quoted.msg.viewOnce) throw 'Reply Pesan Sekali Lihat';
